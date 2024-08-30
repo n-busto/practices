e9422bf200833c452458ed2b65482f76d08c6102
n-busto <nicanor.manuel.busto@gmail.com>
2024-08-30 12:43:04 +0200

    fix properties parsing to object with env vars use

--- mongo/src/main/java/com/nbusto/mongodb/module/PropertiesModule.java
+++ mongo/src/main/java/com/nbusto/mongodb/module/PropertiesModule.java
@@ -1,35 +1,31 @@
 package com.nbusto.mongodb.module;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.nbusto.mongodb.config.Properties;
 import dagger.Module;
 import dagger.Provides;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.env.EnvScalarConstructor;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
 
 @Module
 public interface PropertiesModule {
 
-    // TODO property object method does not replace env vars
     @Provides
-    static Properties getPropertiesClass() {
-        final var mapper = new ObjectMapper(new YAMLFactory());
+    static Properties getPropertiesFromMap() {
+        final var mapper = new ObjectMapper();
 
         try {
-            return mapper.readValue(getInputStream(), Properties.class);
-        } catch (IOException e) {
+            return mapper.convertValue(getProperties(), Properties.class);
+        }catch (IllegalArgumentException e) {
             e.printStackTrace();
         }
 
         return null;
     }
 
-    @Provides
     static Map<String, Object> getProperties() {
         final var yaml = new Yaml(new EnvScalarConstructor());
         yaml.addImplicitResolver(EnvScalarConstructor.ENV_TAG, EnvScalarConstructor.ENV_FORMAT, "$");
