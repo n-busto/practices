88bd017149b35e50399bb5c80ba91177144db66f
n-busto <nicanor.manuel.busto@gmail.com>
2024-09-11 17:33:46 +0200

    add find and retrieve operations

--- mongo/src/main/java/com/nbusto/mongodb/Peak.java
+++ mongo/src/main/java/com/nbusto/mongodb/Peak.java
@@ -1,22 +1,32 @@
 package com.nbusto.mongodb;
 
 import com.mongodb.MongoException;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
 import com.nbusto.mongodb.module.MapperModule;
 import com.nbusto.mongodb.module.MongoModule;
 import com.nbusto.mongodb.module.PropertiesModule;
 import com.nbusto.mongodb.module.ServicesModule;
+import com.nbusto.mongodb.services.find.MongoFindService;
 import com.nbusto.mongodb.services.ping.PingService;
+import com.nbusto.mongodb.services.retrieve.MongoRetrieveService;
 import dagger.Component;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
 import javax.inject.Singleton;
 
 public class Peak {
+    private final static Bson QUERY = Filters.eq("account_id", "1");
+
     public static void main(String[] args) {
         try {
             final var result = DaggerPeak_MongoSettings.builder().build().pingService().ping();
             System.out.println("Pinged your deployment. You successfully connected!");
             System.out.println(result);
+
+            System.out.println(DaggerPeak_MongoSettings.builder().build().mongoFindService().find(QUERY));
+            System.out.println(DaggerPeak_MongoSettings.builder().build().mongoRetrieveService().retrieve(QUERY));
         } catch (MongoException e) {
             e.printStackTrace();
         }
@@ -30,5 +40,7 @@ public class Peak {
             MapperModule.class})
     protected interface MongoSettings {
         PingService<Document> pingService();
+        MongoFindService mongoFindService();
+        MongoRetrieveService mongoRetrieveService();
     }
 }
